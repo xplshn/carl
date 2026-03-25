9483147c1c8d0e957b6d265bb9e0c38c4ca5a49c
xplshn <anto@xplshn.com.ar>
2026-03-25 04:51:02 +0000

    Import Curl 8.17.0

--- lib/curl_setup.h
+++ lib/curl_setup.h
@@ -722,7 +722,6 @@
 
 #if defined(USE_GNUTLS) || defined(USE_OPENSSL) || defined(USE_MBEDTLS) || \
   defined(USE_WOLFSSL) || defined(USE_SCHANNEL) || \
-  defined(USE_BEARSSL) || \
   defined(USE_RUSTLS)
 #define USE_SSL    /* SSL support has been enabled */
 #endif
